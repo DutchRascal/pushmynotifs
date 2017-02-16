3fb8345f45c5bb0a345c04e6610a71cdf31ce835
DutchRascal <Andre@imac.fritz.box>
2017-02-16 10:10:59 +0100

    Initial Commit

---
+++ pushmynotifs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pushmynotifs
+//
+//  Created by Andre Boevink on 16/02/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
